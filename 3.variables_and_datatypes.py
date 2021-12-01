f7fafa4550114d5e8e7c7d6de1be9b07ec042ca5
ShivanshMishra2324 <shivanshmishra2324@gmail.com>
2021-12-01 23:22:34 +0530

    Variables and Datatypes in Python

---
+++ 3.variables_and_datatypes.py
@@ -0,0 +1,10 @@
+character_name="george" #sting
+character_age="35" #string
+number = 67 #int
+ismale = True #boolean
+print("there once was a man named george,")
+print("there once was a man named "+character_name+ ",")
+print("he was " +character_age+ " years old")
+character_age="36"
+print("he was " +character_age+ " years old")
+print("he really like the name george")
