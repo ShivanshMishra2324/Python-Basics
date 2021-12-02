0a72ce0cc19966a246a8900f78e6d4d9225335f4
ShivanshMishra2324 <shivanshmishra2409@gmail.com>
2021-12-02 23:39:18 +0530

    Getting input from user.

---
+++ 6.Getting_input_from_user.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+#Above we took the value that user inputs and we stored in the variable named as name. 
+age = input("Enter your age:")
+print("Hello " + name + " ! You are " + age + " years old" )
+
