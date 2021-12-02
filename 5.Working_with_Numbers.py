78612149f5ff9c699c71f9ce351856969a4e5f0d
ShivanshMishra2324 <shivanshmishra2409@gmail.com>
2021-12-02 23:36:47 +0530

    Working with Numbers

---
+++ 5.Working_with_Numbers.py
@@ -0,0 +1,21 @@
+print(2.34)
+print(2.34+3)
+print(2*3%1-2)
+num = 5
+print(num)
+#convert number  to string
+print(str(num)+" my favourite number") #Without converting it will return a TypeError i.e.
+# print(str(num)+ my favourite mnumber) will give error
+num=-6
+print(abs(num)) #this will give absolute value of a num
+print(pow(3, 2))
+print(max(4, 26))
+print(min(4, 26))
+print(round(4.26))
+print(round(4.5))
+print(round(4.53))
+#down whatever funtion you will see is imported
+from math import * #math is module
+print(floor(4.68))
+print(ceil(4.68))
+print(sqrt(4))
