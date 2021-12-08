d6fa284ba40439ca7d23a27ec2a9251d8af41759
ShivanshMishra2324 <shivanshmishra2409@gmail.com>
2021-12-08 23:51:18 +0530

    Tuples.

---
+++ 11.Tuple.py
@@ -0,0 +1,8 @@
+coordinates = (4,5)
+#Index         0 1
+#Tuples are immmutable i.e. we cannot modify it in anyway.
+print(coordinates[1])
+##python will give error for coordinates[1]=6
+#Down here is list of tuples and we can't modify these tuples as stated earlier.
+coordinate = [(3,4) , (4,5)]
+print(coordinate[1])
