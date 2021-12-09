27a6f045f0f43e430475f7c18746bf4a8cc475bd
ShivanshMishra2324 <shivanshmishra2409@gmail.com>
2021-12-10 00:58:02 +0530

    Functions in python

---
+++ 12.Functions.py
@@ -0,0 +1,6 @@
+def sayhi(name=input("Enter your name: ")): #Name of Function  should be in lowercase.
+    print("Hello User : "+name)
+
+print("top")
+sayhi() #Here it jumps up and execute the code in function then jumps down to execute the futher lines
+print("bottom")   
